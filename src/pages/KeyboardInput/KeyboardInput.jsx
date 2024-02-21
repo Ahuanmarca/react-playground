459684b0a71cba712e3d24376315b864b1f7e583
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-21 17:09:19 +0100

    Update KeyboardInput page so input disappears after 1 second (unless new keys are pressed).

--- src/pages/KeyboardInput/KeyboardInput.jsx
+++ src/pages/KeyboardInput/KeyboardInput.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import './KeyboardInput.css';
+import Nav from "react-bootstrap/Nav";
 
 function KeyboardInput() {
   const [input, setInput] = React.useState({
@@ -23,17 +23,36 @@ function KeyboardInput() {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, []);
 
+  React.useEffect(() => {
+    if(input.code !== "") {
+      const timeout = window.setTimeout(() => {
+        setInput((prevInput) => {
+          const nextInput = {...prevInput};
+          nextInput.key = "";
+          nextInput.code = "";
+          return nextInput;
+        })
+      }, 500);
+      return () => window.clearTimeout(timeout);
+    }
+  }, [input])
+
   return (
     <>
       <h1>Keyboard Input</h1>
       <p>
         This component shows the keyboard input by adding an event listener on
-        the window object.
+        the window object. We do this inside a useEffect hook, which returns a
+        clean up function to remove the event listener when the component
+        unmounts.
       </p>
       <p>
-        <a target="blank" href="https://github.com/Ahuanmarca/react-playground/blob/fc9533b6a3cf59bf945d94c80998b907fd2eb306/src/pages/KeyboardInput/KeyboardInput.jsx">
-          code
-        </a>
+        <Nav.Link
+          target="blank"
+          href="https://github.com/Ahuanmarca/react-playground/blob/fc9533b6a3cf59bf945d94c80998b907fd2eb306/src/pages/KeyboardInput/KeyboardInput.jsx"
+        >
+          🔗 code
+        </Nav.Link>
       </p>
       <h2>Key: {input.key}</h2>
       <h2>Code: {input.code}</h2>
