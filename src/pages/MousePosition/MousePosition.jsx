4fe35c639676d30304df17a02b9630ebc1c91926
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-21 17:08:46 +0100

    Implement MousePosition page.

--- src/pages/MousePosition/MousePosition.jsx
+++ src/pages/MousePosition/MousePosition.jsx
@@ -1,10 +1,46 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+
 function MousePosition() {
+  const [mousePosition, setMousePosition] = React.useState({
+    x: 0,
+    y: 0,
+  });
+
+  React.useEffect(() => {
+    function handleMouseMove(e) {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    }
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <>
-    <h1>Show Mouse Position</h1>
-    <h2>TODO</h2>
+      <h1>Show Mouse Position</h1>
+      <p>
+        This component shows the mouse position by adding a
+        &rsquo;mousemove&rsquo; event listener to the window object. We do this
+        inside a useEffect hook, then return a clean up function to remove the
+        event listener when the component unmounts.
+      </p>
+      <p>
+        <Nav.Link
+          href="https://github.com/Ahuanmarca/react-playground/blob/b7a7cfaa917096354776bad0de7a5d0723734a53/src/pages/MousePosition/MousePosition.jsx"
+          target="blank"
+        >
+          🔗 code
+        </Nav.Link>
+      </p>
+      <h2>
+        {mousePosition.x} / {mousePosition.y}
+      </h2>
     </>
-  )
+  );
 }
 
 export default MousePosition;
