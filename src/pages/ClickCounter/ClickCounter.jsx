c879660ab504701cdeca8e25861e42270e0c1560
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-22 06:10:17 +0100

    Add link to code (repo) on ClickCounter page

--- src/pages/ClickCounter/ClickCounter.jsx
+++ src/pages/ClickCounter/ClickCounter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
 
 function ClickCounter() {
   const [clicks, setClicks] = React.useState(0);
@@ -17,8 +18,15 @@ function ClickCounter() {
         <strong>The total is also displayed on the browser tab.</strong> We
         achieve this with useEffect and useRef.
       </p>
+      <p>
+        <Nav.Link
+          href="https://github.com/Ahuanmarca/react-playground/blob/51331f2a7d063b5a6f7b550811a04fe153369b99/src/pages/ClickCounter/ClickCounter.jsx"
+          target="blank"
+        >
+          code
+        </Nav.Link>
+      </p>
       <h2>Clicks: {clicks}</h2>
-      
       <Button onClick={() => setClicks(clicks + 1)} variant="primary">
         Click me!
       </Button>{" "}
