d651fcbaf126de6fd639f60bab6c342c322262af
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-22 07:14:29 +0100

    Add links to code pages on Timers

--- src/pages/DriftingTimers/TimerIntervalOnMount/TimerIntervalOnMount.jsx
+++ src/pages/DriftingTimers/TimerIntervalOnMount/TimerIntervalOnMount.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 function TimerIntervalOnMount({ speed }) {
   const [count, setCount] = React.useState(0);
@@ -16,6 +17,14 @@ function TimerIntervalOnMount({ speed }) {
       <h2>
         Timer with <code>setInterval</code>: {count}
       </h2>
+      <p>
+        <Nav.Link
+          href="https://github.com/Ahuanmarca/react-playground/blob/1b1b6107f7d8c0d412ba2624cc88656df83a492e/src/pages/DriftingTimers/TimerIntervalOnMount/TimerIntervalOnMount.jsx"
+          target="blank"
+        >
+          code
+        </Nav.Link>
+      </p>
       <p>
         This component adds an <code>setInterval</code> to the window object{" "}
         <strong>only then the component is mounted.</strong> A clean up function
--- src/pages/DriftingTimers/TimerTimeout/TimerTimeout.jsx
+++ src/pages/DriftingTimers/TimerTimeout/TimerTimeout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 function TimerTimeout({ speed }) {
   const [count, setCount] = React.useState(0);
@@ -16,6 +17,14 @@ function TimerTimeout({ speed }) {
       <h2>
         Timer with <code>setTimeout</code>: {count}
       </h2>
+      <p>
+        <Nav.Link
+          href="https://github.com/Ahuanmarca/react-playground/blob/1b1b6107f7d8c0d412ba2624cc88656df83a492e/src/pages/DriftingTimers/TimerTimeout/TimerTimeout.jsx"
+          target="blank"
+        >
+          code
+        </Nav.Link>
+      </p>
       <p>
         This component adds an <code>setTimeout</code> to the window object.{" "}
         <strong>The timeout needs to be added on every render.</strong> A clean
