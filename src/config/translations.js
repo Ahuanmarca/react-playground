92d31c3df1a0db30cf31ad4fa7d305cc68fbd0ba
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-24 19:18:25 +0100

    Bugfix on translations.js caused by invalid browser language

--- src/config/translations.js
+++ src/config/translations.js
@@ -3,8 +3,13 @@ import { initReactI18next } from "react-i18next";
 import spanishTranslations from "./translations/es-ES";
 import englishTranslations from "./translations/en-US";
 
+const VALID_LANGUAGES = ["es-ES", "en-US"];
+const FALLBACK_LANGUAGE = "es-ES";
+
 i18next.use(initReactI18next).init({
-  lng: navigator.language || "es-ES",
+  lng: VALID_LANGUAGES.includes(navigator.language)
+    ? navigator.language
+    : FALLBACK_LANGUAGE,
   resources: {
     "es-ES": { translation: spanishTranslations },
     "en-US": { translation: englishTranslations },
--- src/pages/LanguageSelector/LanguageSelector.jsx
+++ src/pages/LanguageSelector/LanguageSelector.jsx
@@ -1,17 +1,9 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 import Container from "react-bootstrap/Container";
 import Select from "../../components/Select";
 
 function LanguageSelector() {
   const { t, i18n } = useTranslation();
-
-  React.useEffect(() => {
-    const currentLang = i18n.language;
-    const validLangs = Object.keys(i18n.options.resources);
-    const isValidLang = validLangs.includes(currentLang);
-    if (!isValidLang) i18n.changeLanguage("en-US");
-  }, [i18n]);
 
   function formatSelectData() {
     const validLangs = Object.keys(i18n.options.resources);
