165551e18e89b8459ad018dc3272e95baaea9379
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-24 19:17:23 +0100

    Add sidebar without functionality, to test distribution

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 import Home from "../../pages/Home";
 import KeyboardInput from "../../pages/KeyboardInput";
 import MousePosition from "../../pages/MousePosition";
@@ -14,18 +15,21 @@ function App() {
   return (
     <>
       <Navbar />
-      <Container className="mt-3">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/keyboardinput" element={<KeyboardInput />} />
-          <Route path="/showmouseposition" element={<MousePosition />} />
-          <Route path="/sortabletable" element={<SortableTable />} />
-          <Route path="/clickcounter" element={<ClickCounter />} />
-          <Route path="/driftingtimers" element={<DriftingTimers />} />
-          <Route path="/loginform" element={<LoginForm />} />
-          <Route path="/languageselector" element={<LanguageSelector />} />
-        </Routes>
-      </Container>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div className="mt-3" style={{ paddingLeft: "20px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/keyboardinput" element={<KeyboardInput />} />
+            <Route path="/showmouseposition" element={<MousePosition />} />
+            <Route path="/sortabletable" element={<SortableTable />} />
+            <Route path="/clickcounter" element={<ClickCounter />} />
+            <Route path="/driftingtimers" element={<DriftingTimers />} />
+            <Route path="/loginform" element={<LoginForm />} />
+            <Route path="/languageselector" element={<LanguageSelector />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
