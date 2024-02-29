26ef4c6ad6df8a366a4c322c5803c5a80c99c33d
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-29 06:18:00 +0100

    Add Josh Comeau's custom CSS reset!

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/authContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/authContext';
 import './config/translations';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './reset.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
